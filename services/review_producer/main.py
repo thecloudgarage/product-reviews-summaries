3f34ba3ecb559d899536e87d4bc67a448b700d20
Ambar Hassani <39495790+thecloudgarage@users.noreply.github.com>
2026-05-29 12:56:39 +0530

    Update main.py

--- services/review_producer/main.py
+++ services/review_producer/main.py
@@ -1,23 +1,21 @@
 import random
 import time
-from elasticsearch import Elasticsearch
 from kafka import KafkaProducer
-from common.utils import env, fake_review, logger, to_json_bytes
+from common.utils import build_elasticsearch_client, env, fake_review, logger, to_json_bytes
 
 log = logger("review_producer")
 
-ES_URL = env("ELASTICSEARCH_URL", required=True)
 PRODUCTS_INDEX = env("PRODUCTS_INDEX", "products")
 BOOTSTRAP = env("KAFKA_BOOTSTRAP_SERVERS", required=True)
 TOPIC = env("TOPIC_PRODUCT_REVIEWS", "product-reviews")
 INTERVAL = float(env("PRODUCE_INTERVAL_SECONDS", "1"))
 
-def load_products(es: Elasticsearch):
+def load_products(es):
     res = es.search(index=PRODUCTS_INDEX, size=1000, query={"match_all": {}})
     return [hit["_source"] for hit in res["hits"]["hits"]]
 
 def main():
-    es = Elasticsearch(ES_URL)
+    es = build_elasticsearch_client()
     products = load_products(es)
     if not products:
         raise RuntimeError("No products found. Run product_seeder first.")
